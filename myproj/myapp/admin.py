224d6569d62b877683e6309b2b58bdafb1feb2dd
Schadrack Cineas <sjcineas@gmail.com>
2023-02-20 20:16:01 -0500

    first commit

--- myproj/myapp/admin.py
+++ myproj/myapp/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+@admin.register(Instructor)
+class InstructorAdmin(admin.ModelAdmin):
+        list_display=['instructor_id', 'first_name', 'last_name', 'email','phone_num', 'course']
+
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display= ['course_id', 'course_name', 'instructor']
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'student_id', 'parent', 'year_in_school']    
+
+@admin.register(Parent)
+class ParentAdmin(admin.ModelAdmin):
+    list_display= ['parent_id', 'first_name', 'last_name', 'email', 'phone_num']
+
+# @admin.register(Schedule)
+# class ScheduleAdmin(admin.ModelAdmin):
+#     list_display = ['student_id', 'course1', 'course2', 'course3', 'course4', 'course5', 'course6']
--- myproj/myapp/models.py
+++ myproj/myapp/models.py
@@ -1,3 +1,95 @@
+from email.policy import default
+from pyexpat import model
 from django.db import models
+from random import randint
+
+studentID = randint(1000000,9999999)
+parentID = randint(1000000,9999999)
+instructorID = randint(1000000,9999999)
+courseID = randint(1000,9999)
+
+class Parent(models.Model):
+    #---and student need modifications---
+    parent_id = models.IntegerField(default=parentID, unique=True)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone_num = models.IntegerField()
+    #parent should recieve parent ID from parent Table
+    #parent can have many children
+    
+
+    def __str__(self):
+        return "%s %s" % (self.first_name, self.last_name)
+    
 
 # Create your models here.
+class Student(models.Model):
+    #--- and Parent need modifications---
+    student_id = models.IntegerField(default=studentID, unique=True)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    #parent should recieve parent ID from parent Table
+    #child can have many parents
+    parent = models.ForeignKey(Parent, on_delete=models.CASCADE)
+
+    YEAR_IN_SCHOOL_CHOICES = [
+    ('FR', 'Freshman'),
+    ('SO', 'Sophomore'),
+    ('JR', 'Junior'),
+    ('SR', 'Senior'),
+    ('GR', 'Graduated'),
+    ]
+    year_in_school = models.CharField(
+        max_length=2,
+        choices=YEAR_IN_SCHOOL_CHOICES,
+        default='FR'
+    )
+
+    def __str__(self):
+        return "%s %s %s" % (self.student_id, self.first_name, self.last_name)
+
+    class Meta:
+        ordering = ['student_id', 'first_name', 'last_name']
+    
+
+class Instructor(models.Model):
+    #--- and course need modifications---
+    instructor_id = models.IntegerField(default=instructorID, unique=True)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    phone_num = models.IntegerField()
+    #instructor can have many courses
+
+
+    def __str__(self):
+        return "%s %s %s %s" % (self.first_name, self.last_name, self.email, self.phone_num)
+
+class Course(models.Model):
+    #---Instructor needs modifications---
+    course_id = models.IntegerField(default=courseID)
+    course_name = models.CharField(max_length=100)
+    # time = models.TimeField()
+    #Course can have many instructor
+    instructor = models.ForeignKey(Instructor, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.course_name
+
+    class Meta:
+        ordering = ['course_name']
+
+class Schedule(models.Model):
+    #--everything needs modification---
+    #One student to one schedule
+    student_id =models.ForeignKey(Student, on_delete=models.CASCADE)
+    #each from a course
+    course_1 = models.ForeignKey(Course, on_delete=models.CASCADE)
+    
+
+    def __str__(self):
+        return "%s" % (self.course_1)
+
+    class Meta:
+        ordering = ['course_1']
